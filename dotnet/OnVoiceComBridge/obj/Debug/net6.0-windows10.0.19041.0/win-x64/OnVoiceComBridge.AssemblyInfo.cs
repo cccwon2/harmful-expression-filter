7d2169c7c1fc3966d5aeaad2e7a9ebec6cffc3c9
Won Kim <cccwon79@gmail.com>
2025-11-26 17:15:31 +0900

    빌드 오류 수정

--- dotnet/OnVoiceComBridge/obj/Debug/net6.0-windows10.0.19041.0/win-x64/OnVoiceComBridge.AssemblyInfo.cs
+++ dotnet/OnVoiceComBridge/obj/Debug/net6.0-windows10.0.19041.0/win-x64/OnVoiceComBridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnVoiceComBridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b04405155edc87f3318c8ce5d6da235d9e2ac026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b41a68510fd75f32d7d3b6fb3fa9f4ccaea453a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnVoiceComBridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnVoiceComBridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
